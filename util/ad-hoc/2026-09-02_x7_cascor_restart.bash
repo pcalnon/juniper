e2785056855cafe0be067dcd0922effe7ef96797
Paul Calnon <paul.calnon@gmail.com>
2026-09-03 06:37:22 -0500

    fixed empty assignment for LD_LIBRARY_PATH env var that didn't play nicely with the multi-line command

--- util/ad-hoc/2026-09-02_x7_cascor_restart.bash
+++ util/ad-hoc/2026-09-02_x7_cascor_restart.bash
@@ -18,9 +18,9 @@ UVICORN=/opt/miniforge3/envs/JuniperCascor1/bin/uvicorn
 
 cd "${CASCOR_SRC}" || exit 1
 
-LD_LIBRARY_PATH= \
-JUNIPER_DATA_URL=http://127.0.0.1:8105 \
-JUNIPER_CASCOR_WS_CONTROL_ALLOWED_ORIGINS=http://127.0.0.1:8055 \
+LD_LIBRARY_PATH="" \
+JUNIPER_DATA_URL="http://127.0.0.1:8105" \
+JUNIPER_CASCOR_WS_CONTROL_ALLOWED_ORIGINS="http://127.0.0.1:8055" \
 nohup "${UVICORN}" api.app:create_app --factory --host 127.0.0.1 --port 8206 \
     >> "${RUN_DIR}/logs/juniper-cascor.log" 2>&1 &
 
